58a9d1a96e9b7183e48f33a907acff9bb5856d80
JLTC3111 <85354982+JLTC3111@users.noreply.github.com>
2025-04-25 17:29:52 +0700

    Add files via upload

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,14 +1,14 @@
 export function Tabs(props) {
     const { todos, selectedTab, setSelectedTab } = props
     
-    const tabs = ['Tất Cả', 'Chưa Hoàn Thành', 'Đã Xong']
+    const tabs = ['Tất Cả', 'Chưa Làm', 'Đã Làm']
 
     return (
         <nav className="tab-container">
             {tabs.map((tab, tabIndex) => {
-                const numOfTasks = tab === 'All' ? 
+                const numOfTasks = tab === 'Tất Cả' ? 
                 todos.length :
-                tab === 'Incomplete' ?
+                tab === 'Chưa Làm' ?
                         todos.filter(val => !val.complete).length : 
                         todos.filter(val => val.complete).length
 
